11512a916b5c35a9bc4ec90e153eb7a3a89af592
piecemakerz <piecemakerz@naver.com>
2020-06-18 19:08:26 +0900

    fixing cors

--- src/assets/js/generateModel.js
+++ src/assets/js/generateModel.js
@@ -3,26 +3,21 @@ const imagesInput = document.getElementById("modelGeneration__file");
 const userId = document.getElementById("modelGeneration__userId");
 const submitBtn = document.getElementById("modelGeneration__submit");
 
-const handleGenerationStart = async (event) => {
+const handleGenerationStart = async () => {
   const file = imagesInput.files[0];
   const userid = userId.value;
   const formdata = new FormData();
   formdata.append("file", file, file.name);
   formdata.append("userid", userid);
-  try {
-    const response = await fetch("https://fitting-home.fun25.co.kr/upload", {
-      method: "POST",
-      body: formdata,
-    });
-    const responseText = await response.text();
-    if (response.status === 200) {
-      alert(responseText);
-      window.location.href = `${window.location.origin}/me`;
-    } else {
-      alert(responseText);
-    }
-  } catch (error) {
-    console.log(error);
+
+  const response = await fetch("https://fitting-home.fun25.co.kr/upload", {
+    method: "POST",
+    body: formdata,
+  });
+  const responseText = await response.text();
+  alert(responseText);
+  if (response.status === 200) {
+    window.location.href = `${window.location.origin}/me`;
   }
 };
 
