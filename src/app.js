cb0c286ad2ee59e87414d91e7d4ff22f347b2495
piecemakerz <piecemakerz@naver.com>
2020-06-18 14:15:24 +0900

    solving cors error

--- src/app.js
+++ src/app.js
@@ -24,7 +24,20 @@ import apiRouter from "./routers/apiRouter";
 import "./passport";
 
 const app = express();
-
+app.use(cors());
+app.use(function(req, res, next) {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://florismeininger.herokuapp.com"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+  res.setHeader("Access-Control-Allow-Credentials", true);
+  next();
+});
 const CookieStore = MongoStore(session);
 
 app.use(helmet());
@@ -42,7 +55,6 @@ app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
-app.use(cors());
 
 app.use(
   session({
