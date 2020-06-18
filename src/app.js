4daa4e2eddc632af6cef0b39f079614f2e85efe6
piecemakerz <piecemakerz@naver.com>
2020-06-18 14:34:12 +0900

    solving cors problem

--- src/app.js
+++ src/app.js
@@ -25,19 +25,6 @@ import "./passport";
 
 const app = express();
 app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader(
-    "Access-Control-Allow-Origin",
-    "https://fitting-home.fun25.co.kr"
-  );
-  res.setHeader("Access-Control-Allow-Methods", "POST, OPTIONS");
-  res.setHeader(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With,content-type"
-  );
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 const CookieStore = MongoStore(session);
 
 app.use(helmet());
